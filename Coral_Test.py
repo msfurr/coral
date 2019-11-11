52d41c3f2dd98594fe1b1795add7bf44f77ba90c
Mike <msfurr94@gmail.com>
2019-11-11 13:04:53 -0500

    updates

--- Coral_Test.py
+++ Coral_Test.py
@@ -13,65 +13,6 @@ import numpy as np
 from tflite_runtime.interpreter import Interpreter
 import time
 import pandas as pd
-
-# Create moving average to remove single outliers
-def movingAvg(Class, windowSize):
-    
-    filteredClass = []
-    for i in range(0, len(Class)):
-        
-        if i < windowSize - 1:        
-            filteredClass.append(Class[i])
-            
-        elif Class[i] != Class[i - 1]:
-            
-            if sum(Class[i - (windowSize - 1):(i + 1)]) / windowSize > 1:            
-                filteredClass.append(2)
-                
-            elif sum(Class[i - (windowSize - 1):(i + 1)]) / windowSize < 1:            
-                filteredClass.append(0)
-                
-            else:            
-                filteredClass.append(Class[i])
-                
-        elif Class[i] == Class[i - 1]:        
-            filteredClass.append(Class[i])
-        
-    return filteredClass
-
-def classSwitch(Class):
-
-    decision = []
-    switchLog = []
-    for i in range(0, len(Class)):
-        
-        if i >= 1:
-            
-            if Class[i] != Class[i - 1]:        
-    
-                # Log when it changes to 1            
-                if Class[i] == 1:
-                    switchLog.append(i)
-                    
-                # If it changes to 0 or 2, add to final    
-                if Class[i] == 2 or Class[i] == 0:
-                    decision.append(Class[i])
-            
-            # If the values continue to be 1, change to value before switch
-            if Class[i] == 1:    
-                decision.append(Class[switchLog[-1] - 1])
-                
-            # If the value does not change and it is not 1, add to final  
-            elif Class[i] == Class[i - 1] and Class[i] != 1:     
-                decision.append(Class[i])
-                
-        # Add first value to final
-        else: 
-            decision.append(Class[i])
-            
-    # Return the moving average with a window of 3 for the final list
-    # to remove jumps between classes
-    return movingAvg(decision, 3)
     
 #%%
 
@@ -82,7 +23,7 @@ def main():
     data = np.float32([data])
     
     # Setup interpreter for inference
-    interpreter = Interpreter(model_path="model_4.tflite")
+    interpreter = Interpreter(model_path = "model_4.tflite")
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
