777cd4e58eaa6017aaa812511936a8f88b1effd2
Mike <msfurr94@gmail.com>
2019-11-12 14:19:47 -0500

    Update Read_Write_Sensor.py

--- Read_Write_Sensor.py
+++ Read_Write_Sensor.py
@@ -262,6 +262,7 @@ for i in range(0, 5000):
                 if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.5:
                     Max_1 = min(RawData_1[-RescaleRange:-1]) + 800
                     print('RESCALED')
+                    time.sleep(2)
 
                 if 1 - max(Sensor_2_Data[-RescaleRange:-1]) > 0.5:
                     Max_2 = min(Sensor_2_Data[-RescaleRange:-1]) + 1500
