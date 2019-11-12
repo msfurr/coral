84c21854864ef086b2deebed49e5c0fc3c46f646
Mike <msfurr94@gmail.com>
2019-11-12 14:42:24 -0500

    Update Read_Write_Sensor.py

--- Read_Write_Sensor.py
+++ Read_Write_Sensor.py
@@ -259,9 +259,9 @@ for i in range(0, 5000):
             
             if j > RescaleRange and Max_1 - Min_1 != 0:
 
-                if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.5:
+                if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.7:
                     print(Max_1)
-                    Max_1 = Max_1 - 600
+                    Max_1 = Max_1 - 500
 
                 if Sensor_1 > 0.99:
                     Max_1 = Max_1 + 50
