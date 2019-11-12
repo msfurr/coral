959db86e6eb3df04ed91da8bbec2a146907ca38e
Mike <msfurr94@gmail.com>
2019-11-12 12:22:47 -0500

    Update Scaling_Test.py

--- Scaling_Test.py
+++ Scaling_Test.py
@@ -258,28 +258,28 @@ for i in range(0, 10000):
             if len(Sensor_1_Data) > RescaleRange:
 
                 if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.7 and Sensor_1 < 0.45:
-                    Max_1 = Max_1 - 100
+                    Max_1 = Max_1 - 50
 
                 if 1 - max(Sensor_2_Data[-RescaleRange:-1]) > 0.5 and Sensor_2 < 0.45:
-                    Max_2 = Max_2 - 100
+                    Max_2 = Max_2 - 50
 
                 if 1 - max(Sensor_3_Data[-RescaleRange:-1]) > 0.5 and Sensor_3 < 0.45:
-                    Max_3 = Max_3 - 100
+                    Max_3 = Max_3 - 50
 
                 if 1 - max(Sensor_4_Data[-RescaleRange:-1]) > 0.5 and Sensor_4 < 0.45:
-                    Max_4 = Max_4 - 100
+                    Max_4 = Max_4 - 50
 
                 if min(Sensor_1_Data[-RescaleRange_Max:-1]) > 0.8 and Sensor_1 > 0.2:
-                    Max_1 = Max_1 + 100
+                    Max_1 = Max_1 + 20
 
                 if min(Sensor_2_Data[-RescaleRange_Max:-1]) > 0.8 and Sensor_2 > 0.2:
-                    Max_2 = Max_2 + 100
+                    Max_2 = Max_2 + 20
 
                 if min(Sensor_3_Data[-RescaleRange_Max:-1]) > 0.8 and Sensor_3 > 0.2:
-                    Max_3 = Max_3 + 100
+                    Max_3 = Max_3 + 20
 
                 if min(Sensor_4_Data[-RescaleRange_Max:-1]) > 0.8 and Sensor_4 > 0.2:
-                    Max_4 = Max_4 + 100
+                    Max_4 = Max_4 + 20
 
             # Gather values for displaying
             values[0] = Sensor_1
