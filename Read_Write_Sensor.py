c6a958b1ef05445341d2f6d20843366d7f2547c1
msfurr <43248171+msfurr@users.noreply.github.com>
2019-11-12 10:40:02 -0500

    Update Read_Write_Sensor.py

--- Read_Write_Sensor.py
+++ Read_Write_Sensor.py
@@ -89,15 +89,15 @@ for i in range(0, 50000):
 
         # At the end of data gathering set the min and max of the range
         if i == 10:
-            Min_1 = min(Range_1) - 800
-            Min_2 = min(Range_2) - 800
-            Min_3 = min(Range_3) - 800
-            Min_4 = min(Range_4) - 800
+            Min_1 = min(Range_1) - 1000
+            Min_2 = min(Range_2) - 1000
+            Min_3 = min(Range_3) - 1000
+            Min_4 = min(Range_4) - 1000
 
-            Max_1 = min(Range_1) + 1200
-            Max_2 = min(Range_2) + 1200
-            Max_3 = min(Range_3) + 1200
-            Max_4 = min(Range_4) + 1200
+            Max_1 = min(Range_1) + 1500
+            Max_2 = min(Range_2) + 1500
+            Max_3 = min(Range_3) + 1500
+            Max_4 = min(Range_4) + 1500
 
     # Begin data collection with scaling
     else:
@@ -257,16 +257,16 @@ for i in range(0, 50000):
             if len(Sensor_1_Data) > RescaleRange:
 
                 if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.2:
-                    Max_1 = min(RawData_1[-RescaleRange:-1]) + 1200
+                    Max_1 = min(RawData_1[-RescaleRange:-1]) + 1500
 
                 if 1 - max(Sensor_2_Data[-RescaleRange:-1]) > 0.2:
-                    Max_2 = min(RawData_2[-RescaleRange:-1]) + 1200
+                    Max_2 = min(RawData_2[-RescaleRange:-1]) + 1500
 
                 if 1 - max(Sensor_3_Data[-RescaleRange:-1]) > 0.2:
-                    Max_3 = min(RawData_3[-RescaleRange:-1]) + 1200
+                    Max_3 = min(RawData_3[-RescaleRange:-1]) + 1500
 
                 if 1 - max(Sensor_4_Data[-RescaleRange:-1]) > 0.2:
-                    Max_4 = min(RawData_4[-RescaleRange:-1]) + 1000
+                    Max_4 = min(RawData_4[-RescaleRange:-1]) + 1500
 
             # Gather values for displaying
             values[0] = Sensor_1
