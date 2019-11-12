27799f9c8f949fee8d50dd4f11802456ee1fd286
Mike <msfurr94@gmail.com>
2019-11-12 11:59:29 -0500

    Update Scaling_Test.py

--- Scaling_Test.py
+++ Scaling_Test.py
@@ -283,8 +283,8 @@ for i in range(0, 10000):
             # Debugging for real time scaling changes
             print(Sensor_1)
             print('~~~~~~')
-            print('Min_1')
-            print('Max_1')
+            print(Min_1)
+            print(Max_1)
             #print(Min_1)
             #print(Max_1)
             #print('-----')
