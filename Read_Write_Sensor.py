b5e4b2731aff83f29632282507bd4038b3d556d9
msfurr <43248171+msfurr@users.noreply.github.com>
2019-11-13 16:41:00 -0500

    Update Read_Write_Sensor.py

--- Read_Write_Sensor.py
+++ Read_Write_Sensor.py
@@ -253,23 +253,33 @@ for i in range(0, 5000):
 
             # If the min / max of the last 20 data points is far away from the current min / max, rescale
 
-            """
+
             if len(Sensor_1_Data) > RescaleRange:
 
-                if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.5:
-                    Max_1 = max(RawData_1[-RescaleRange:-1]) + 250
+                if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_1_Data[-RescaleRange:-1]) < 0.1:
+                    Max_1 = max(RawData_1[-RescaleRange:-1]) + 500
 
-                if 1 - max(Sensor_2_Data[-RescaleRange:-1]) > 0.5 or 1 - max(Sensor_2_Data[-RescaleRange:-1]) < 0.1:
-                    Max_2 = max(RawData_2[-RescaleRange:-1]) + 250
+                if 1 - max(Sensor_2_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_2_Data[-RescaleRange:-1]) < 0.1:
+                    Max_2 = max(RawData_2[-RescaleRange:-1]) + 500
 
-                if 1 - max(Sensor_3_Data[-RescaleRange:-1]) > 0.5 or 1 - max(Sensor_3_Data[-RescaleRange:-1]) < 0.1:
-                    Max_3 = max(RawData_3[-RescaleRange:-1]) + 250
+                if 1 - max(Sensor_3_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_3_Data[-RescaleRange:-1]) < 0.1:
+                    Max_3 = max(RawData_3[-RescaleRange:-1]) + 500
 
-                if 1 - max(Sensor_4_Data[-RescaleRange:-1]) > 0.5 or 1 - max(Sensor_4_Data[-RescaleRange:-1]) < 0.1:
-                    Max_4 = max(RawData_4[-RescaleRange:-1]) + 250
+                if 1 - max(Sensor_4_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_4_Data[-RescaleRange:-1]) < 0.1:
+                    Max_4 = max(RawData_4[-RescaleRange:-1]) + 500
 
-            """
-                
+                if min(Sensor_1_Data[-RescaleRange:-1]) < 0.15:
+                    Min_1 = min(RawData_1[-RescaleRange:-1]) - 500
+
+                if min(Sensor_2_Data[-RescaleRange:-1]) < 0.15:
+                    Min_2 = min(RawData_2[-RescaleRange:-1]) - 500
+
+                if min(Sensor_3_Data[-RescaleRange:-1]) < 0.15:
+                    Min_3 = min(RawData_3[-RescaleRange:-1]) - 500
+
+                if min(Sensor_4_Data[-RescaleRange:-1]) < 0.15:
+                    Min_4 = min(RawData_4[-RescaleRange:-1]) - 500
+
 
             # Gather values for displaying
             values[0] = Sensor_1
