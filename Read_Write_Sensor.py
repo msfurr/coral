abbe09c3431d755d5850ea39557002ad6138df24
Mike <msfurr94@gmail.com>
2019-11-17 16:49:28 -0500

    Update Read_Write_Sensor.py

--- Read_Write_Sensor.py
+++ Read_Write_Sensor.py
@@ -258,27 +258,43 @@ for i in range(0, 5000):
 
                 if 1 - max(Sensor_1_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_1_Data[-RescaleRange:-1]) < 0.1:
                     Max_1 = max(RawData_1[-RescaleRange:-1]) + 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if 1 - max(Sensor_2_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_2_Data[-RescaleRange:-1]) < 0.1:
                     Max_2 = max(RawData_2[-RescaleRange:-1]) + 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if 1 - max(Sensor_3_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_3_Data[-RescaleRange:-1]) < 0.1:
                     Max_3 = max(RawData_3[-RescaleRange:-1]) + 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if 1 - max(Sensor_4_Data[-RescaleRange:-1]) > 0.2 or 1 - max(Sensor_4_Data[-RescaleRange:-1]) < 0.1:
                     Max_4 = max(RawData_4[-RescaleRange:-1]) + 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if min(Sensor_1_Data[-RescaleRange:-1]) < 0.15:
                     Min_1 = min(RawData_1[-RescaleRange:-1]) - 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if min(Sensor_2_Data[-RescaleRange:-1]) < 0.15:
                     Min_2 = min(RawData_2[-RescaleRange:-1]) - 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if min(Sensor_3_Data[-RescaleRange:-1]) < 0.15:
                     Min_3 = min(RawData_3[-RescaleRange:-1]) - 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
                 if min(Sensor_4_Data[-RescaleRange:-1]) < 0.15:
                     Min_4 = min(RawData_4[-RescaleRange:-1]) - 300
+                    print('SCALE CHANGE')
+                    time.sleep(2)
 
             
 
